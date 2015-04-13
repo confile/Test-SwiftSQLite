b51dba32b783d505511d3fe27a2e09d0537b2d92
Michael Gorski <info@michaelgorski.de>
2015-04-13 14:39:26 +0200

    init

---
+++ TestSQLite/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  TestSQLite
+//
+//  Created by Michael Gorski on 13.04.15.
+//  Copyright (c) 2015 Michael Gorski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ TestSQLite/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestSQLite
+//
+//  Created by Michael Gorski on 13.04.15.
+//  Copyright (c) 2015 Michael Gorski. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
